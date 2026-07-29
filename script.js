3165028d9a85dfd059c9315d405ece141b24ff10
Dutton1-lang <Dutton_1@sky.com>
2026-07-29 22:22:50 +0100

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-
-document.querySelectorAll(".faq-button").forEach((button) => {
-  button.addEventListener("click", () => {
-    const answer = button.nextElementSibling;
-    const isOpen = answer.classList.toggle("open");
-    button.querySelector("span").textContent = isOpen ? "⌃" : "⌄";
-  });
-});
